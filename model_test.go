78c75826f99290b287e9cbb87f73358d9d9d19cf
Aine <aine@etke.cc>
2024-02-22 22:34:59 +0200

    v1.0.0

--- model_test.go
+++ model_test.go
@@ -2,18 +2,34 @@ package msc1929
 
 import "testing"
 
-var testResponse = &Response{
-	Contacts:    []Contact{{Email: "contact@example.com", MatrixID: "@contact:example.com"}},
-	Admins:      []Contact{{Email: "admin@example.com"}},
-	SupportPage: "https://example.com",
-	hasContent:  true,
-	emails:      []string{"contact@example.com", "admin@example.com"},
-	mxids:       []string{"@contact:example.com"},
-}
+var (
+	testResponseOK = &Response{
+		Contacts: []*Contact{
+			{Email: "admin@example.com", MatrixID: "@admin:example.com", Role: RoleAdmin},
+			{Email: "security@example.com", MatrixID: "@security:example.com", Role: RoleSecurity},
+			{Email: "just@example.com", MatrixID: "@just:example.com", Role: "some-other-role"},
+			{Email: "missing-role@example.com", MatrixID: "@missing-role:example.com"},
+			{MatrixID: "@missing-email:example.com", Role: "missing-email"},
+			{Email: "missing-mxid@example.com", Role: "missing-mxid"},
+			{Role: "missing-email-mxid"},
+			{},
+		},
+		SupportPage: "https://example.com",
+	}
+	testResponseInvalid = &Response{
+		Contacts: []*Contact{
+			{Email: "@invalid:email", MatrixID: "invalid-mxid"},
+		},
+		SupportPage: "invalid url;://",
+	}
+)
 
 func TestIsEmpty(t *testing.T) {
-	if testResponse.IsEmpty() {
-		t.Fail()
+	if testResponseOK.Clone().IsEmpty() {
+		t.Error("expected non-empty response from testResponseOK, got empty response")
+	}
+	if !testResponseInvalid.Clone().IsEmpty() {
+		t.Errorf("expected empty response from testResponseInvalid, got %+v", testResponseInvalid)
 	}
 }
 
@@ -30,27 +46,49 @@ func TestIsEmpty_Nil(t *testing.T) {
 	}
 }
 
-func TestResponse(t *testing.T) {
-	emails := []string{"contact@example.com", "admin@example.com"}
-	mxids := []string{"@contact:example.com"}
+func TestResponseValid(t *testing.T) {
+	adminEmails := []string{"admin@example.com"}
+	adminMxids := []string{"@admin:example.com"}
+	securityEmails := []string{"security@example.com"}
+	securityMxids := []string{"@security:example.com"}
+	allEmails := []string{"admin@example.com", "security@example.com", "just@example.com", "missing-role@example.com", "missing-mxid@example.com"}
+	allMXIDs := []string{"@admin:example.com", "@security:example.com", "@just:example.com", "@missing-role:example.com", "@missing-email:example.com"}
 	supportPage := "https://example.com"
-	resp := *testResponse
-	resp.hydrate()
 
-	if resp.IsEmpty() {
-		t.Fail()
+	r := testResponseOK.Clone()
+
+	if r.SupportPage != supportPage {
+		t.Errorf("expected support page %s, got %s", supportPage, r.SupportPage)
 	}
-	if supportPage != resp.SupportPage {
-		t.Fail()
+	slicesEqual(t, r.AdminEmails(), adminEmails)
+	slicesEqual(t, r.AdminMatrixIDs(), adminMxids)
+	slicesEqual(t, r.SecurityEmails(), securityEmails)
+	slicesEqual(t, r.SecurityMatrixIDs(), securityMxids)
+	slicesEqual(t, r.AllEmails(), allEmails)
+	slicesEqual(t, r.AllMatrixIDs(), allMXIDs)
+}
+
+func TestResponseInvalid(t *testing.T) {
+	r := testResponseInvalid.Clone()
+	r.Sanitize()
+
+	if len(r.Contacts) != 0 {
+		t.Errorf("expected no contacts, got %d", len(r.Contacts))
 	}
-	for i, email := range resp.Emails() {
-		if emails[i] != email {
-			t.Fail()
-		}
+	if r.SupportPage != "" {
+		t.Errorf("expected empty support page, got %s", r.SupportPage)
 	}
-	for i, mxid := range resp.MatrixIDs() {
-		if mxids[i] != mxid {
-			t.Fail()
+}
+
+func slicesEqual(t *testing.T, a, b []string) {
+	t.Helper()
+
+	if len(a) != len(b) {
+		t.Errorf("slices %+v and %+v have different lengths: %d and %d", a, b, len(a), len(b))
+	}
+	for i, v := range a {
+		if b[i] != v {
+			t.Errorf("slices %+v and %+v differ at index %d: %s and %s", a, b, i, v, b[i])
 		}
 	}
 }
